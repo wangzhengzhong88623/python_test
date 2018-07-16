9a9fe1d3758fee24bef87f03361393178398ae1c
root <root@localhost.localdomain>
2018-07-16 06:39:37 -0400

    0716

--- host_test/urls.py
+++ host_test/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from host_test import views
 urlpatterns = [
     url(r'^business/$', views.business),
+    url(r'^host/$', views.host),
 ]
 
--- host_test/views.py
+++ host_test/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect,HttpResponse
 from django.shortcuts import render
 from django.views import View
 from host_test import models
 def business(request):
     v = models.Business.objects.all()
     return render(request,'business.html',{'v':v})
+
+
+
+
+def host(request):
+    v1 = models.Host_test.objects.filter(nid__gt=0)
+    for row in v1:
+        print(row.nid,row.hostname,row.ip,row.port,row.b_id,row.b.caption,row.b.code,sep='\t')
+    return render(request,'host.html',{'v1':v1})
